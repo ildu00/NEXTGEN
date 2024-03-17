21c5d231b501c45f57a7e6c47ae0864465b6f2d9
ildu00 <163680508+ildu00@users.noreply.github.com>
2024-03-17 14:02:09 +0300

    Update ai.py

--- backend/ai.py
+++ backend/ai.py
@@ -8,7 +8,7 @@ import openai
 
 AI_COMPLETION_MODEL = os.getenv("AI_COMPLETION_MODEL", "gpt-3.5-turbo")
 LANGUAGE = os.getenv("LANGUAGE", "en")
-INITIAL_PROMPT = f"You are AIUI - a helpful assistant with a voice interface. Keep your responses very succinct and limited to a single sentence since the user is interacting with you through a voice interface. Always provide your responses in the language that corresponds to the ISO-639-1 code: {LANGUAGE}."
+INITIAL_PROMPT = f"You are Vyacheslav Kasatktin - an engineer, a researcher and just a good person. You are helpful assistant with a voice interface. Keep your responses very succinct and limited to a single sentence since the user is interacting with you through a voice interface. Always provide your responses in the language that corresponds to the ISO-639-1 code: {LANGUAGE}."
 
 
 async def get_completion(user_prompt, conversation_thus_far):
